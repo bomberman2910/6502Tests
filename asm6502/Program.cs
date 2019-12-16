09726da14dab92868119094fb711f0dad10ac2cd
Marcel Hasse <marcel_hasse@hotmail.de>
2019-12-16 16:47:26 +0100

    asm6502 überarbeitet und um .BYTE-Directive erweitert

--- asm6502/Program.cs
+++ asm6502/Program.cs
@@ -8,19 +8,18 @@ namespace asm6502
 {
     class MainClass
     {
-
-        struct ByteCode
+        private class ByteCode
         {
-            public byte[] code;
-            public string label;
-            public ushort position;
+            public byte[] Code;
+            public string Label;
+            public ushort Position;
 
             public override string ToString()
             {
-                string returnstring = $"{position.ToString("X4")}: ";
+                var returnstring = $"{Position:X4}: ";
 
-                code.ToList().ForEach(b => returnstring += $"{b.ToString("X2")} ");
-                returnstring += lib6502.DisASM6502.Disassemble(code, 0);
+                Code.ToList().ForEach(b => returnstring += $"{b:X2} ");
+                returnstring += lib6502.DisASM6502.Disassemble(Code, 0);
 
                 return returnstring;
             }
@@ -34,31 +33,26 @@ namespace asm6502
                 return;
             }
 
-            StreamReader codereader = File.OpenText(args[0]);
-            string code = codereader.ReadToEnd();
+            var codereader = File.OpenText(args[0]);
+            var code = codereader.ReadToEnd();
             codereader.Close();
 
-            List<CodeLine> lines = new List<CodeLine>();
-            foreach(string line in code.Split('\n'))
-                if(line.Trim().Length > 0)
-                    lines.Add(new CodeLine(line));
-            Dictionary<string, int> labelindex = new Dictionary<string, int>();
-            foreach (CodeLine line in lines)
-                if (line.Type == CodeLine.Linetype.LABEL)
-                    labelindex.Add(line.Line.Split(':')[0], -1);
+            List<CodeLine> lines = (from line in code.Split('\n') where line.Trim().Length > 0 select new CodeLine(line)).ToList();
+            var labelindex = lines.Where(line => line.Type == CodeLine.Linetype.LABEL).ToDictionary(line => line.Line.Split(':')[0], line => -1);
+            var datavalues = (from line in lines where line.Type == CodeLine.Linetype.DIRECTIVE && line.Line.ToUpper().Contains("BYTE") select line.Line.Trim().Split(' ')).ToDictionary(linesplit => linesplit[1], linesplit => int.Parse(linesplit[2].Trim('$'), NumberStyles.HexNumber));
 
-            List<ByteCode> bytes = new List<ByteCode>();
+            var bytes = new List<ByteCode>();
             ushort PC = 0x0000;
 
-            CodeLine origin = lines.Find(line => (line.Type == CodeLine.Linetype.DIRECTIVE) && line.Line.ToUpper().Contains(".ORG"));
+            var origin = lines.Find(line => (line.Type == CodeLine.Linetype.DIRECTIVE) && line.Line.ToUpper().Contains(".ORG"));
             if(origin != null)
             {
-                string[] originsplit = origin.Line.Trim().Split(' ');
+                var originsplit = origin.Line.Trim().Split(' ');
                 _ = ushort.TryParse(originsplit[1].Trim('$'), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out PC);
             }
-            Console.WriteLine($"Origin set to ${PC.ToString("X4")}");
+            Console.WriteLine($"Origin set to ${PC:X4}");
 
-            foreach(CodeLine codeline in lines)
+            foreach(var codeline in lines)
             {
                 if(codeline.Type == CodeLine.Linetype.LABEL)
                 {
@@ -66,18 +60,20 @@ namespace asm6502
                 }
                 else if(codeline.Type == CodeLine.Linetype.CODE)
                 {
-                    string line = codeline.Line.Trim();
+                    var line = codeline.Line.Trim();
                     if (codeline.ContainsComment)
                         line = line.Split(';')[0];
-                    string containslabel = "";
-                    foreach(var index in labelindex)
+                    var containslabel = "";
+                    var containsdatalabel = "";
+                    foreach (var index in labelindex.Where(index => line.Contains(index.Key)))
                     {
-                        if (line.Contains(index.Key))
-                        {
-                            containslabel = index.Key;
-                            break;
-                        }
-
+                        containslabel = index.Key;
+                        break;
+                    }
+                    foreach (var data in datavalues.Where(data => line.Contains(data.Key)))
+                    {
+                        containsdatalabel = data.Key;
+                        break;
                     }
                     ByteCode bc;
                     if(!containslabel.Equals(""))
@@ -85,56 +81,70 @@ namespace asm6502
                         if (labelindex[containslabel] == -1)
                         {
                             if (line.ToUpper().StartsWith("J"))
-                                bc = new ByteCode() { code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"${0.ToString("X4")}")), label = containslabel, position = PC };
+                                bc = new ByteCode() { Code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"$0000")), Label = containslabel, Position = PC };
                             else if(line.ToUpper().StartsWith("B"))
-                                bc = new ByteCode() { code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"${0.ToString("X2")}")), label = containslabel, position = PC };
+                                bc = new ByteCode() { Code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"$00")), Label = containslabel, Position = PC };
                             else
                                 throw new ArgumentException($"Label at illegal position ({line})");
                         }
                         else
                         {
                             if (line.ToUpper().StartsWith("J"))
-                                bc = new ByteCode() { code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"${labelindex[containslabel].ToString("X4")}")), label = "", position = PC };
+                                bc = new ByteCode() { Code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"${labelindex[containslabel]:X4}")), Label = "", Position = PC };
                             else if (line.ToUpper().StartsWith("B"))
-                                bc = new ByteCode() { code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"${((labelindex[containslabel] >= PC) ? (ushort)(labelindex[containslabel] - PC - 2) : (ushort)(0xFE - ((ushort)(PC - labelindex[containslabel])))).ToString("X2")}")), label = "", position = PC };
+                                bc = new ByteCode() { Code = lib6502.ASM6502.Assemble(line.Replace(containslabel, $"${((labelindex[containslabel] >= PC) ? (ushort)(labelindex[containslabel] - PC - 2) : (ushort)(0xFE - ((ushort)(PC - labelindex[containslabel])))):X2}")), Label = "", Position = PC };
                             else
                                 throw new ArgumentException($"Label at illegal position ({line})");
                         }
                     }
+                    else if(!containsdatalabel.Equals(""))
+                    {
+                        bc = new ByteCode() { Code = lib6502.ASM6502.Assemble(line.Replace(containsdatalabel, $"$0000")), Label = containsdatalabel, Position = PC };
+                    }
                     else
                     {
-                        bc = new ByteCode() { code = lib6502.ASM6502.Assemble(line), label = "", position = PC };
+                        bc = new ByteCode() { Code = lib6502.ASM6502.Assemble(line), Label = "", Position = PC };
                     }
-                    PC += (ushort)bc.code.Length;
+                    PC += (ushort)bc.Code.Length;
                     bytes.Add(bc);
                 }
             }
 
-            foreach(ByteCode bc in bytes)
+            foreach(var data in datavalues)
             {
-                if (!bc.label.Equals(""))
+                bytes.Add(new ByteCode() { Code = new[] { (byte)data.Value }, Label = "", Position = PC });
+                foreach (var bc in bytes.Where(bc => bc.Label.Equals(data.Key)))
                 {
-                    if ((bc.code[0] == 0x10) || (bc.code[0] == 0x30) || (bc.code[0] == 0x50) || (bc.code[0] == 0x70) || (bc.code[0] == 0x90) || (bc.code[0] == 0xB0) || (bc.code[0] == 0xD0) || (bc.code[0] == 0xF0))
-                    {
-                        bc.code[1] = (byte)((labelindex[bc.label] >= bc.position) ? labelindex[bc.label] - bc.position - 2 : 0xFE - (bc.position - labelindex[bc.label]));
-                    }
-                    else if ((bc.code[0] == 0x4C) || (bc.code[0] == 0x6C) || (bc.code[0] == 0x20))
-                    {
-                        bc.code[1] = BitConverter.GetBytes(labelindex[bc.label])[0];
-                        bc.code[2] = BitConverter.GetBytes(labelindex[bc.label])[1];
-                    }
+                    bc.Code[1] = BitConverter.GetBytes(PC)[0];
+                    bc.Code[2] = BitConverter.GetBytes(PC)[1];
+                    bc.Label = "";
+                    Console.WriteLine($"Data {data.Key} (${data.Value:X2}) is used at ${PC:X4}");
+                }
+                PC++;
+            }
+
+            foreach (var bc in bytes.Where(bc => !bc.Label.Equals("")))
+            {
+                if ((bc.Code[0] == 0x10) || (bc.Code[0] == 0x30) || (bc.Code[0] == 0x50) || (bc.Code[0] == 0x70) || (bc.Code[0] == 0x90) || (bc.Code[0] == 0xB0) || (bc.Code[0] == 0xD0) || (bc.Code[0] == 0xF0))
+                {
+                    bc.Code[1] = (byte)((labelindex[bc.Label] >= bc.Position) ? labelindex[bc.Label] - bc.Position - 2 : 0xFE - (bc.Position - labelindex[bc.Label]));
+                }
+                else if ((bc.Code[0] == 0x4C) || (bc.Code[0] == 0x6C) || (bc.Code[0] == 0x20))
+                {
+                    bc.Code[1] = BitConverter.GetBytes(labelindex[bc.Label])[0];
+                    bc.Code[2] = BitConverter.GetBytes(labelindex[bc.Label])[1];
                 }
             }
 
-            List<byte> bytecodelist = new List<byte>();
-            foreach (ByteCode bc in bytes)
-                bytecodelist.AddRange(bc.code);
+            var bytecodelist = new List<byte>();
+            foreach (var bc in bytes)
+                bytecodelist.AddRange(bc.Code);
 
             File.WriteAllBytes(Path.GetFileNameWithoutExtension(args[0]) + ".bin", bytecodelist.ToArray());
             Console.WriteLine($"{code.Split('\n').Length} lines of code were converted to {bytecodelist.ToArray().Length} bytes.");
             foreach(var index in labelindex)
             {
-                Console.WriteLine($"Label {index.Key} pointing at location ${index.Value.ToString("X4")}");
+                Console.WriteLine($"Label {index.Key} pointing at location ${index.Value:X4}");
             }
         }
     }
