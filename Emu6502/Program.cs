f09b653c2efc5bb7feba353d86fae039fce0845c
Marcel Hasse <marcel_hasse@hotmail.de>
2020-05-01 19:18:22 +0200

    Code refactoring

--- Emu6502/Program.cs
+++ Emu6502/Program.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using lib6502;
 
 namespace Emu6502
 {
-    class MainClass
+    internal static class MainClass
     {
         [DllImport("libc")]
         private static extern int system(string exec);
@@ -18,98 +16,90 @@ namespace Emu6502
         {
             system(@"printf '\e[8;" + height.ToString() + ";" + width.ToString() + "t'");
         }
-        
-        public static void InvertColors()
+
+        private static void InvertColors()
         {
             var temp = Console.ForegroundColor;
             Console.ForegroundColor = Console.BackgroundColor;
             Console.BackgroundColor = temp;
         }
 
-        private static CPU6502 cpu;
-        private static Bus mainbus;
-        private static RAM ram;
-        private static ROM rom;
-        private static Screen screen;
-        private static ROM charrom;
-        private static TextScreen textscreen;
-        private static PIA pia;
-        private static SIA sia;
+        private static CPU6502 _cpu;
+        private static Bus _mainbus;
+        private static RAM _ram;
+        private static ROM _rom;
+        private static Screen _screen;
+        private static TextScreen _textscreen;
+        private static PIA _pia;
+        private static SIA _sia;
+        
+        private static List<ushort> _breakpoints;
+        private static ushort _currentpage;
 
         private static void Reset()
         {
-            mainbus = new Bus();
-            ram = new RAM(4096, 0x0000);
+            _mainbus = new Bus();
+            _ram = new RAM(4096, 0x0000);
             //var bbytes = File.ReadAllBytes("dectest.bin");
             //for (var pc = 0; pc < bbytes.Length; pc++)
             //   ram.SetData(bbytes[pc], (ushort)(0x0200 + pc));
-            mainbus.Devices.Add(ram);
+            _mainbus.Devices.Add(_ram);
 
-            rom = new ROM(4096, 0xF000);
+            _rom = new ROM(4096, 0xF000);
             var initrom = new byte[4096];
             initrom[0x0FFD] = 0x02;
             for (var i = 0; i < ASMRoutines.PixelDspRoutine().Length; i++)
                 initrom[0x0000 + i] = ASMRoutines.PixelDspRoutine()[i];
             for (var i = 0; i < ASMRoutines.CharDspRoutine().Length; i++)
                 initrom[0x001C + i] = ASMRoutines.CharDspRoutine()[i];
-            rom.SetMemory(initrom);
-            mainbus.Devices.Add(rom);
+            _rom.SetMemory(initrom);
+            _mainbus.Devices.Add(_rom);
 
-            screen = new Screen(160, 120, 0xD000);
-            screen.Reset();
-            mainbus.Devices.Add(screen);
+            _screen = new Screen(160, 120, 0xD000);
+            _screen.Reset();
+            _mainbus.Devices.Add(_screen);
 
-            textscreen = new TextScreen(40, 25, 0xD010);
-            textscreen.Reset();
-            mainbus.Devices.Add(textscreen);
+            _textscreen = new TextScreen(40, 25, 0xD010);
+            _textscreen.Reset();
+            _mainbus.Devices.Add(_textscreen);
             
-            pia = new PIA(cpu, 0xD020);
-            mainbus.Devices.Add(pia);
+            _pia = new PIA(_cpu, 0xD020);
+            _mainbus.Devices.Add(_pia);
             
-            sia = new SIA(0xD030);
-            mainbus.Devices.Add(sia);
+            _sia = new SIA(0xD030);
+            _mainbus.Devices.Add(_sia);
 
-            cpu = new CPU6502(mainbus)
+            _cpu = new CPU6502(_mainbus)
             {
                 PC = 0x0200
             };
         }
 
-        public static void Main(string[] args)
+        private static void PrintStatus()
         {
-            ushort currentpage = 0x0000;
-
-            ResizeMac(140, 40);
-            //Console.SetWindowSize(140, 43);
             Console.Clear();
-
-            Reset();
-
-            var command = "";
-            var breakpoints = new List<ushort>();
-
-            while (!command.ToLower().Equals("q"))
+            Console.WriteLine($"{_cpu}\n");
+            Console.WriteLine($"{_pia}\n");
+            for (int line = _currentpage; line < ((_currentpage + 0x0400) > 65536 ? 65536 : (_currentpage + 0x0400)); line += 32)
             {
-                Console.Clear();
-                Console.WriteLine($"{cpu}\n");
-                Console.WriteLine($"{pia}\n");
-                for (int line = currentpage; line < ((currentpage + 0x0400) > 65536 ? 65536 : (currentpage + 0x0400)); line += 32)
+                Console.Write("$" + line.ToString("X4") + ":");
+                for (var pc = line; pc < (line + 32); pc++)
                 {
-                    Console.Write("$" + line.ToString("X4") + ":");
-                    for (var pc = line; pc < (line + 32); pc++)
-                    {
-                        Console.Write(" ");
-                        if (pc == cpu.PC) InvertColors();
-                        Console.Write($"${mainbus.GetData((ushort) pc):X2}");
-                        if (pc == cpu.PC) InvertColors();
-                    }
-
-                    Console.WriteLine();
+                    Console.Write(" ");
+                    if (pc == _cpu.PC) InvertColors();
+                    Console.Write($"${_mainbus.GetData((ushort) pc):X2}");
+                    if (pc == _cpu.PC) InvertColors();
                 }
+
                 Console.WriteLine();
-                Console.Write(">");
-                command = Console.ReadLine().ToLower();
-                ushort usvalue;
+            }
+            Console.WriteLine();
+            Console.Write(">");
+        }
+        
+        private static void ExecuteCommand(string command)
+        {
+            ushort usvalue;
                 byte bvalue;
                 string[] setsplit;
                 var success = false;
@@ -128,32 +118,32 @@ namespace Emu6502
                         if (setsplit[1].Equals("a"))
                         {
                             success = byte.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out bvalue);
-                            if (success) cpu.A = bvalue;
+                            if (success) _cpu.A = bvalue;
                         }
                         else if (setsplit[1].Equals("x"))
                         {
                             success = byte.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out bvalue);
-                            if(success) cpu.X = bvalue;
+                            if(success) _cpu.X = bvalue;
                         }
                         else if (setsplit[1].Equals("y"))
                         {
                             success = byte.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out bvalue);
-                            if(success) cpu.Y = bvalue;
+                            if(success) _cpu.Y = bvalue;
                         }
                         else if (setsplit[1].Equals("sr"))
                         {
                             success = byte.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out bvalue);
-                            if(success) cpu.SR = bvalue;
+                            if(success) _cpu.SR = bvalue;
                         }
                         else if (setsplit[1].Equals("sp"))
                         {
                             success = byte.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out bvalue);
-                            if (success) cpu.SP = bvalue;
+                            if (success) _cpu.SP = bvalue;
                         }
                         else if (setsplit[1].Equals("pc"))
                         {
                             success = ushort.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out usvalue);
-                            if(success) cpu.PC = usvalue;
+                            if(success) _cpu.PC = usvalue;
                         }
 
                         if (success) break;
@@ -171,7 +161,7 @@ namespace Emu6502
                         if (setsplit[1].Equals("disp"))
                         {
                             success = ushort.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out usvalue);
-                            if (success) currentpage = usvalue;
+                            if (success) _currentpage = usvalue;
                         }
                         else if (setsplit[1].Equals("set"))
                         {
@@ -185,7 +175,7 @@ namespace Emu6502
                                 if (!success) break;
                             }
                             for (var mem = 0; mem < bbytes.Length; mem++)
-                                ram.SetData(bbytes[mem], (ushort)(usvalue + mem));
+                                _ram.SetData(bbytes[mem], (ushort)(usvalue + mem));
                         }
                         if (success) break;
                         Console.WriteLine("Fehlerhafte Eingabe!");
@@ -202,7 +192,7 @@ namespace Emu6502
 
                         if (setsplit[1].Equals("list"))
                         {
-                            foreach (var brk in breakpoints)
+                            foreach (var brk in _breakpoints)
                                 Console.Write(brk.ToString("X4") + ", ");
                             _ = Console.ReadKey(true);
                             success = true;
@@ -210,12 +200,12 @@ namespace Emu6502
                         else if (setsplit[1].Equals("add"))
                         {
                             success = ushort.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out usvalue);
-                            if (success) breakpoints.Add(usvalue);
+                            if (success) _breakpoints.Add(usvalue);
                         }
                         else if (setsplit[1].Equals("rm"))
                         {
-                            success = ushort.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out usvalue) && breakpoints.Contains(usvalue);
-                            if (success) breakpoints.Remove(usvalue);
+                            success = ushort.TryParse(setsplit[2], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out usvalue) && _breakpoints.Contains(usvalue);
+                            if (success) _breakpoints.Remove(usvalue);
                         }
                         if (success) break;
                         Console.WriteLine("Fehlerhafte Eingabe!");
@@ -234,36 +224,32 @@ namespace Emu6502
                         {
                             if (setsplit[2].Equals("a"))
                             {
-                                Console.WriteLine($"Port A value: {pia.PORTA}");
+                                Console.WriteLine($"Port A value: {_pia.PORTA}");
                                 success = true;
                             }
                             else if (setsplit[2].Equals("b"))
                             {
-                                Console.WriteLine($"Port B value: {pia.PORTB}");
+                                Console.WriteLine($"Port B value: {_pia.PORTB}");
                                 success = true;
                             }
-                            else
-                                success = false;
                         } 
                         else if (setsplit[1].Equals("set"))
                         {
                             if (setsplit[2].Equals("a"))
                             {
                                 success = byte.TryParse(setsplit[3], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out bvalue);
-                                if (success) pia.PORTA = bvalue;
+                                if (success) _pia.PORTA = bvalue;
                             }
                             else if (setsplit[2].Equals("b"))
                             {
                                 success = byte.TryParse(setsplit[3], NumberStyles.HexNumber, NumberFormatInfo.CurrentInfo, out bvalue);
-                                if (success) pia.PORTB = bvalue;
+                                if (success) _pia.PORTB = bvalue;
                             }
                             else if (setsplit[2].Equals("irq"))
                             {
-                                pia.IRQ = !pia.IRQ;
+                                _pia.IRQ = !_pia.IRQ;
                                 success = true;
                             }
-                            else
-                                success = false;
                         }
                         if (success) break;
                         Console.WriteLine("Fehlerhafte Eingabe!");
@@ -285,16 +271,15 @@ namespace Emu6502
                         }
                         else if (setsplit[1].Equals("cpu"))
                         {
-                            cpu.Reset();
+                            _cpu.Reset();
                             success = true;
                         }
                         else if (setsplit[1].Equals("pia"))
                         {
-                            pia.Reset();
+                            _pia.Reset();
                             success = true;
                         }
-                        else
-                            success = false;
+
                         if (success) break;
                         Console.WriteLine("Fehlerhafte Eingabe!");
                         _ = Console.ReadKey(true);
@@ -314,7 +299,7 @@ namespace Emu6502
                             {
                                 var bytes = ASM6502.Assemble(setsplit[2]);
                                 for (var i = 0; i < bytes.Length; i++)
-                                    mainbus.SetData(bytes[i], (ushort) (cpu.PC + i));
+                                    _mainbus.SetData(bytes[i], (ushort) (_cpu.PC + i));
                                 success = true;
                             }
                             catch
@@ -340,7 +325,7 @@ namespace Emu6502
                             {
                                 var bytes = ASM6502.Assemble(lines);
                                 for (var i = 0; i < bytes.Length; i++)
-                                    mainbus.SetData(bytes[i], (ushort) (cpu.PC + i));
+                                    _mainbus.SetData(bytes[i], (ushort) (_cpu.PC + i));
                                 success = true;
                             }
                             catch
@@ -348,41 +333,59 @@ namespace Emu6502
                                 success = false;
                             }
                         }
-                        else
-                            success = false;
                         if (success) break;
                         Console.WriteLine("Fehlerhafte Eingabe!");
                         _ = Console.ReadKey(true);
                         break;
                     case var cmd when cmd.Equals(""):
-                        cpu.Step();
-                        mainbus.PerformClockActions();
-                        screen.Screenshot();
-                        textscreen.Screenshot();
+                        _cpu.Step();
+                        _mainbus.PerformClockActions();
+                        _screen.Screenshot();
+                        _textscreen.Screenshot();
                         break;
                     case var cmd when cmd.Equals("r"):
-                        if (breakpoints.Count == 0)
+                        if (_breakpoints.Count == 0)
                             do
                             {
-                                cpu.Step();
-                                mainbus.PerformClockActions();
-                            } while (mainbus.GetData(cpu.PC) != 0x00);
+                                _cpu.Step();
+                                _mainbus.PerformClockActions();
+                            } while (_mainbus.GetData(_cpu.PC) != 0x00);
                         else
                             do
                             {
-                                cpu.Step();
-                                mainbus.PerformClockActions();
-                            } while (!breakpoints.Contains(cpu.PC) && mainbus.GetData(cpu.PC) != 0x00);
+                                _cpu.Step();
+                                _mainbus.PerformClockActions();
+                            } while (!_breakpoints.Contains(_cpu.PC) && _mainbus.GetData(_cpu.PC) != 0x00);
 
-                        screen?.Screenshot();
-                        textscreen?.Screenshot();
+                        _screen?.Screenshot();
+                        _textscreen?.Screenshot();
                         break;
                     default:
                         Console.WriteLine("Fehlerhafte Eingabe!");
                         _ = Console.ReadKey(true);
                         break;
                 }
+        }
 
+        public static void Main(string[] args)
+        {
+            _currentpage = 0x0000;
+
+            ResizeMac(140, 40);
+            //Console.SetWindowSize(140, 43);
+            Console.Clear();
+
+            Reset();
+
+            var command = "";
+            _breakpoints = new List<ushort>();
+
+            while (command != null && !command.ToLower().Equals("q"))
+            {
+                PrintStatus();
+                
+                command = Console.ReadLine()?.ToLower();
+                ExecuteCommand(command);
             }
         }
     }
